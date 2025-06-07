4f97d1ab216fba277b6ccd7d564335500335218a
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-06-07 10:56:27 +0700

    Resource Bundle: other country and default/not found

--- src/test/java/dyaz/io/i18n/ResourceBundleTest.java
+++ src/test/java/dyaz/io/i18n/ResourceBundleTest.java
@@ -2,6 +2,7 @@ package dyaz.io.i18n;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ResourceBundleTest {
@@ -10,6 +11,28 @@ public class ResourceBundleTest {
   void testResourceBundle() {
 
     var resourceBundle = ResourceBundle.getBundle("message");
+
+    System.out.println(resourceBundle.getString("hello"));
+    System.out.println(resourceBundle.getString("goodbye"));
+
+  }
+
+  @Test
+  void testResourceBundleID() {
+
+    var indoneisa = new Locale("in", "ID");
+    var resourceBundle = ResourceBundle.getBundle("message", indoneisa);
+
+    System.out.println(resourceBundle.getString("hello"));
+    System.out.println(resourceBundle.getString("goodbye"));
+
+  }
+
+  @Test
+  void testResourceBundleNotFound() {
+
+    var amerika = new Locale("en", "US");
+    var resourceBundle = ResourceBundle.getBundle("message", amerika);
 
     System.out.println(resourceBundle.getString("hello"));
     System.out.println(resourceBundle.getString("goodbye"));
