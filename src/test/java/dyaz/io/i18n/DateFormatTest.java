26f276ba947fa985f0657b335adf0db6315bd99f
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-06-07 11:25:17 +0700

    Format Date and Time: other country and parse

--- src/test/java/dyaz/io/i18n/DateFormatTest.java
+++ src/test/java/dyaz/io/i18n/DateFormatTest.java
@@ -2,8 +2,10 @@ package dyaz.io.i18n;
 
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateFormatTest {
 
@@ -15,5 +17,29 @@ public class DateFormatTest {
 
     var format = dateFormat.format(new Date());
     System.out.println(format);
+  }
+
+  @Test
+  void testDateFormatJapan() {
+    var pattern = "EEEE dd MMMM yyyy";
+    var japan = new Locale("ja", "JP");
+    var dateFormat = new SimpleDateFormat(pattern, japan);
+
+    var format = dateFormat.format(new Date());
+    System.out.println(format);
+  }
+
+  @Test
+  void testDateFormatParseJapan() {
+    var pattern = "EEEE dd MMMM yyyy";
+    var japan = new Locale("ja", "JP");
+    var dateFormat = new SimpleDateFormat(pattern, japan);
+
+    try {
+      var date = dateFormat.parse("土曜日 07 6月 2025");
+      System.out.println(date);
+    } catch (ParseException e) {
+      System.out.println("Error Parse : " + e.getMessage());
+    }
   }
 }
